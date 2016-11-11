3dbc0fce9d643008e868baa03c2491aef5240cad
kawingliu6 <calliu@ymail.com>
2016-11-11 11:20:37 +0800

    update new version

--- javascript/conversation.js
+++ javascript/conversation.js
@@ -6,23 +6,44 @@ function myFunction() {
     x = document.getElementById("chatroom").value.toLowerCase();
     if (x=="hi") {
     document.getElementById("demo").innerHTML += "A: Hello!\n" + '<input type="button" class="voice" name="hello" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
-    if (x=="can i kill justin") {
-    document.getElementById("demo").innerHTML += "A: Go Ahead!"+ '<input type="button" class="voice" name="go ahead" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
-    if (x=="how should i kill him") {
-    document.getElementById("demo").innerHTML += "A: Cut J first!\n"+ '<input type="button" class="voice" name="cut J first" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
-    if (x=="am i handsome") {
-    document.getElementById("demo").innerHTML += "A: Yes!\n"+ '<input type="button" class="voice" name="yes" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
-    if (x=="yo") {
-    document.getElementById("demo").innerHTML += "A: yooooooooooooo!\n"+ '<input type="button" class="voice" name="yoooooooo" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
-    if (x=="hi dickson") {
-    document.getElementById("demo").innerHTML += "A: dicksonnnnnnnnnnnnnnnnnnnn\n"+ '<input type="button" class="voice" name="dickson" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
-    }
     
+    // products
+    //food
+    }else if (x.includes("food a") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food A : "+'<a href="food/foodA.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food A " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("food b") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food B : "+'<a href="food/foodB.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food B " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("food c") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food C : "+'<a href="food/foodC.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food C " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("food d") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food D : "+'<a href="food/foodD.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food D " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("food e") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food E : "+'<a href="food/foodE.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food E " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("food f") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of food F : "+'<a href="food/foodF.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of food F " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    
+    //house
+    }else if (x.includes("house a") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house A : "+'<a href="house/houseA.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house A " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("house b") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house B : "+'<a href="house/houseB.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house B " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("house c") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house C : "+'<a href="house/houseC.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house C " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("house d") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house D : "+'<a href="fhouse/houseD.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house D " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("house e") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house E : "+'<a href="house/houseE.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house E " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }else if (x.includes("house f") ){ 
+    document.getElementById("demo").innerHTML += "A: Here is the product page of house F : "+'<a href="house/houseF.html">link</a>'+"\n"+ '<input type="button" class="voice" name="Here is the product page of house F " value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+
+    //medic
+    
+    //other
+    
+    //error message
+    }else{
+    document.getElementById("demo").innerHTML += "A: Sorry, I dont understand. \n"+ '<input type="button" class="voice" name="Sorry, I dont understand" value="Play voice!" onClick="return testFunction(this)" />'+"</br>";
+    }
     
     document.getElementById("chatroom").value="";
 }
---
+++ javascript/search.js
@@ -0,0 +1,52 @@
+  var searchProduct = function(event) {
+    
+    var myNode = document.getElementById("result");
+    while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+    }
+    
+    var output = document.getElementById('preview');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    var target=event.target.files[0].name;
+    var img = new Array(5);
+    if (target.includes("other")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/other'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    if (target.includes("food")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/food'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    if (target.includes("house")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/house'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    if (target.includes("medic")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/medic'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    if (target.includes("cloth")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/cloth'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+}
