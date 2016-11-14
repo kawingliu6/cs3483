bce8d42206a3bb5e023db2c46b9e2ef722d43b99
kawingliu6 <calliu@ymail.com>
2016-11-14 11:11:11 +0800

    update search function of index.html & search.html

--- javascript/search.js
+++ javascript/search.js
@@ -50,3 +50,118 @@
         }
     }
 }
+
+  var searchProductByText = function() {
+    
+    var myNode = document.getElementById("result");
+    while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+    }
+    
+    var searchText = document.getElementById('search_bar').value;
+    
+    var image = new Image();
+    image.src = 'image/'+searchText+'.jpg';
+    image.style.height = '200px';
+    document.getElementById('result').appendChild(image);
+    
+    image.onerror = function () {
+
+    myNode.removeChild(myNode.firstChild);
+    
+    var img = new Array(5);
+    if (searchText.includes("other")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/other'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    else if (searchText.includes("food")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/food'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    else if (searchText.includes("house")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/house'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    else if (searchText.includes("medic")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/medic'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }
+    else if (searchText.includes("cloth")) {
+        for(i=0;i<6;i++){
+        img[i] = new Image();
+        img[i].src = 'image/cloth'+String.fromCharCode(65+i)+'.jpg';
+        img[i].style.height = '200px';
+        document.getElementById('result').appendChild(img[i]);
+        }
+    }else{
+      document.getElementById('result').innerHTML="no result found";
+    }
+    }
+}
+
+  window.onload = function() {
+        if (window.location.href.includes("?")) {
+          var url = window.location.href.split("?");
+          var searchText=url[1];
+            
+          var img = new Array(5);
+          if (searchText.includes("other")) {
+              for(i=0;i<6;i++){
+              img[i] = new Image();
+              img[i].src = 'image/other'+String.fromCharCode(65+i)+'.jpg';
+              img[i].style.height = '200px';
+              document.getElementById('result').appendChild(img[i]);
+              }
+          }
+          else if (searchText.includes("food")) {
+              for(i=0;i<6;i++){
+              img[i] = new Image();
+              img[i].src = 'image/food'+String.fromCharCode(65+i)+'.jpg';
+              img[i].style.height = '200px';
+              document.getElementById('result').appendChild(img[i]);
+              }
+          }
+          else if (searchText.includes("house")) {
+              for(i=0;i<6;i++){
+              img[i] = new Image();
+              img[i].src = 'image/house'+String.fromCharCode(65+i)+'.jpg';
+              img[i].style.height = '200px';
+              document.getElementById('result').appendChild(img[i]);
+              }
+          }
+          else if (searchText.includes("medic")) {
+              for(i=0;i<6;i++){
+              img[i] = new Image();
+              img[i].src = 'image/medic'+String.fromCharCode(65+i)+'.jpg';
+              img[i].style.height = '200px';
+              document.getElementById('result').appendChild(img[i]);
+              }
+          }
+          else if (searchText.includes("cloth")) {
+              for(i=0;i<6;i++){
+              img[i] = new Image();
+              img[i].src = 'image/cloth'+String.fromCharCode(65+i)+'.jpg';
+              img[i].style.height = '200px';
+              document.getElementById('result').appendChild(img[i]);
+              }
+          }else{
+            document.getElementById('result').innerHTML="no result found";
+          }
+        }
+  }
