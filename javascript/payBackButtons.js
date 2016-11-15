a6744e044d353019117e7f3b75f5136670c4bd27
kawingliu6 <calliu@ymail.com>
2016-11-15 09:19:40 +0800

    update payment page

---
+++ javascript/payBackButtons.js
@@ -0,0 +1,7 @@
+        function goToPayment(){
+            location.href="../payment.html?"+document.getElementById("productImage").alt;
+        }
+        
+        function goBack(){
+            window.history.back();
+        }
